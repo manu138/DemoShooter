d670359471ec127f0d6616e08a68afe2b58a799d
juan camilo rios <juan22415@hotmail.com>
2017-05-02 02:56:47 -0500

    disparo dragon

---
+++ Assets/shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject bala;
+    private float timer=0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+       
+    }
+
+    public void dragonshoot ()
+    {
+        GameObject municion = Instantiate(bala, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+        Rigidbody rigBala = municion.GetComponent<Rigidbody>();
+        rigBala.AddForce(gameObject.transform.forward * 5000);
+        timer = 3;
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (timer<0)
+            {
+                dragonshoot();
+                timer = 3;
+            }
+           
+        }
+    }
+}
